9eabe49cfafb1f7536328129e0d1ff7fd8c5e3b2
absolus2 <absolus24@gmail.com>
2022-07-18 21:31:51 -0400

    basic gameplay

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -4,6 +4,8 @@ class Game
     @list = list
     @rand_word 
     @player_word
+    @guessed_letters
+    @letter_turn
   end
 
   def get_word
@@ -15,12 +17,12 @@ class Game
   end
 
   def play
-    try = 0
     intro
-    while try != @tries
+    while @tries != 0
+      p "Tries left #{@tries}"
       turn
-      p @rand_word
-      try += 1
+      @tries -= 1 if check_try(@letter_turn) != true
+      p "Game over, You lose!, The word was #{@rand_word.split("")}" if @tries == 0
     end
   end
 
@@ -29,16 +31,39 @@ class Game
     p "Hello, this the hangman game The game will choose a random word and you'll have to guess correctly"
     p "You have 8 tries, every turn the game will ask you for a letter if the letter is in the word you can keep going"
     p "if the letter does't match you'll lose a try. Good luck!"
+    @guessed_letters = Array.new(@rand_word.length).map { |item| item = '-'}
   end
 
   def turn
     p "What's the letter for this turn?"
-    letter = gets.chomp 
-    p "letter was #{letter}"
+    letter = gets.chomp.downcase
+    @letter_turn = letter
+    populate_guess(letter)
+    p @guessed_letters
+    game_over
+  end
+
+
+  def populate_guess(letter)
+    @rand_word.split("").each_with_index do |item, index|
+      @guessed_letters[index] = item  if item == letter
+    end
+  end
+
+  def game_over
+    if @rand_word.split("") == @guessed_letters
+      p "You WON!! Congratz!"
+      sleep(5)
+    end
+  end
+
+  def check_try(letter)
+    true if @rand_word.count(letter) >= 1
   end
 end
 
 new_game = Game.new(8, 'words_list.txt')
 new_game.play
+
 
 
